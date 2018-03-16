c7ba2413678cb3f742ea918e9a093d8b36c6fc2d
AaronStPete <AaronStPete@users.noreply.github.com>
2018-03-16 09:48:58 -0400

    built createPlayer1() which changes playerName from input field

--- public/main.js
+++ public/main.js
@@ -1,31 +1,70 @@
 //create environment logic
-  //pick # of players (low)
+//pick # of players (low)
 
 // create a player object
+function player(name, hand,) {
   //player name
-  //player score (low)
+  this.name = ""
   //contains hand
+  this.hand = []
+  //player score (low)
+}
+
+//creating a player
+//enter a name using <input>
+const createPlayer1 = (input) => {
+  let playerName = document.querySelector("#playerName");
+  input = document.querySelector("#playerNameField").value
+  playerName = input;
+  console.log(playerName);
+  document.querySelector("#playerName").textContent = playerName;
+  //hide #playerNameField && #playerNameButton
+  let hiddenField = document.querySelector("#playerNameField");
+  hiddenField.classList = "hide";
+  let hiddenButton = document.querySelector("#playerNameButton");
+  hiddenButton.classList = "hide";
+}
+
+// const card = {
+//   key: "";
+//   value: "" ;
+//   suit: ;
+// }
+
+
 
 // house logic
 
-// create game logic
-  //create a deck
-    //card values
-      //(1-9)*4
-      //(10,jack,queen,king)*4
-      //(aces)*4 (basevalue=11)
-        //(ace) (basevalue=11 or 1) (low)
-    // [deck > [card] > [value, suit, playedThisRound]]
-  //how to hit
-    // if hit => addAvailableCard()
-  //check if bust
-  // if hand > 21 => lose()
-  // win()
-    // log win in game history
-    // celebrate announcement
+draw = () => {
+  card
+}
+//create a deck
+  // const deck 
+// = Math.ceil((Math.random()*53));
+  //#'s 1-13 spades
+  //#'s 14-26 hearts
+  //#'s 27-39 diamonds
+  //#'s 40-52 clubs
+//card values
+//(1-9)*4
+//(10,jack,queen,king)*4
+//(aces)*4 (basevalue=11)
+//(ace) (basevalue=11 or 1) (low)
+// [deck > [card] > [key, value, suit, playedThisRound]] - unavailable cards
+//how to hit()
+// if hit => addAvailableCard()
+//check if bust
+// if hand > 21 => lose()
+// win()
+// log win in game history
+// celebrate announcement
 
 //new hand reset
-  //dealNewHand()
+const dealNewHand = () => {
+  draw()
+  draw()
+  //stay or hit?
+}
 
 //new game reset
   //newGame()
