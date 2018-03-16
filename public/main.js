b463c0fca24ee241e1160e24105c721f32b3a436
AaronStPete <AaronStPete@users.noreply.github.com>
2018-03-16 15:16:53 -0400

    built shuffleDeck() which places cardDeck into availableCards

--- public/main.js
+++ public/main.js
@@ -1,14 +1,4 @@
 //create environment logic
-//pick # of players (low)
-
-// create a player object
-function player(name, hand,) {
-  //player name
-  this.name = ""
-  //contains hand
-  this.hand = []
-  //player score (low)
-}
 
 //creating a player
 //enter a name using <input>
@@ -23,48 +13,80 @@ const createPlayer1 = (input) => {
   hiddenField.classList = "hide";
   let hiddenButton = document.querySelector("#playerNameButton");
   hiddenButton.classList = "hide";
+  //store new name as player1.name
+  player1.name = document.querySelector("#playerName").textContent
 }
 
-// const card = {
-//   key: "";
-//   value: "" ;
-//   suit: ;
-// }
+const testClick = () => {
+  console.log("this button is working")
+  console.log("player1.name = " + player1.name);
+  console.log("player1's hand is: " + player1.hand);
+}
 
+const player1 = {
+  name: "",
+  hand: []
+}
 
+const cardDeck = {
+  key: "",
+  value: "",
+  suit: "",
+}
 
-// house logic
+const shuffleDeck = () => {
+  // this function takes cardDeck and places it into available cards
+availableCards = cardDeck
+console.log("The deck has been reshuffled")
+}
+
+let availableCards = [];
 
 draw = () => {
-  card
+  //add available card to player1.hand
 }
-//create a deck
-  // const deck 
-// = Math.ceil((Math.random()*53));
-  //#'s 1-13 spades
-  //#'s 14-26 hearts
-  //#'s 27-39 diamonds
-  //#'s 40-52 clubs
-//card values
-//(1-9)*4
-//(10,jack,queen,king)*4
-//(aces)*4 (basevalue=11)
-//(ace) (basevalue=11 or 1) (low)
-// [deck > [card] > [key, value, suit, playedThisRound]] - unavailable cards
-//how to hit()
-// if hit => addAvailableCard()
-//check if bust
-// if hand > 21 => lose()
-// win()
-// log win in game history
-// celebrate announcement
-
+  
 //new hand reset
 const dealNewHand = () => {
   draw()
   draw()
   //stay or hit?
 }
+  
+  //create a deck
+  // const deck 
+  // = Math.ceil((Math.random()*53));
+  //#'s 1-13 spades
+  //#'s 14-26 hearts
+  //#'s 27-39 diamonds
+  //#'s 40-52 clubs
+  //card values
+  //(1-9)*4
+  //(10,jack,queen,king)*4
+  //(aces)*4 (basevalue=11)
+  //(ace) (basevalue=11 or 1) (low)
+  // [deck > [card] > [key, value, suit, playedThisRound]] - unavailable cards
+  //how to hit()
+  // if hit => addAvailableCard()
+  //check if bust
+  // if hand > 21 => lose()
+  // win()
+  // log win in game history
+  // celebrate announcement
+  
+  
+  // house logic
+  
+  
+///////pick # of players (low)
+// create a player object
+// function player(name, hand,) {
+  //   //player name
+  //   this.name = ""
+  //   //contains hand
+  //   this.hand = []
+  //   //player score (low)
+  // }
 
-//new game reset
-  //newGame()
+//new game reset button
+//newGame()
